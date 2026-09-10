f731f964cac7c5e14bd9d9b26df76a9d0fafc7c4
YanAllets <yanstellaofc@gmail.com>
2026-09-09 21:38:59 -0300

    Remove database config from repository(Again)

--- DataBase/DBConfig.cs
+++
@@ -1,8 +0,0 @@
-using MySqlConnector;
-
-namespace ExpenseTrackerApi.DataBase;
-public class Config
-{
-    public static string conexao = "Server=localhost;Database=expensetracker;User ID=root;Password=Spooky-Velocity-Unlearned;";
-    public static MySqlConnection conn = new MySqlConnection(conexao);
-}
