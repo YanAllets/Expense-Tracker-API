a95fb64393043918b8f39e09ad9785a42c7c92c1
YanAllets <yanstellaofc@gmail.com>
2026-09-09 21:24:04 -0300

    Remove database credentials from repository

--- DataBase/DBConfig.cs
+++
@@ -1,8 +0,0 @@
-using MySqlConnector;
-
-namespace ExpenseTrackerApi.DataBase;
-public class Config
-{
-    public static string conexao = "Server=localhost;Database=expensetracker;User ID=root;Password=Hypnotize-Overrule-Luckiness7;";
-    public static MySqlConnection conn = new MySqlConnection(conexao);
-}
